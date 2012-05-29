37ab81081ce21feed90057cd0cb7b0202481d390
Rick van bodegraven <rickvanbodegraven@gmail.com>
2012-05-29 15:07:28 +0200

    converterbean

---
+++ java6eetut/src/converter/ejb/ConverterBean.java
@@ -0,0 +1,20 @@
+package converter.ejb;
+
+import java.math.BigDecimal;
+import javax.ejb.*;
+
+@Stateless
+public class ConverterBean {
+    private BigDecimal yenRate = new BigDecimal("83.0602");
+    private BigDecimal euroRate = new BigDecimal("0.0093016");
+
+    public BigDecimal dollarToYen(BigDecimal dollars) {
+        BigDecimal result = dollars.multiply(yenRate);
+        return result.setScale(2, BigDecimal.ROUND_UP);
+    }
+
+    public BigDecimal yenToEuro(BigDecimal yen) {
+        BigDecimal result = yen.multiply(euroRate);
+        return result.setScale(2, BigDecimal.ROUND_UP);
+    }
+}
