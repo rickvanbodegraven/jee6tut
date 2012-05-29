ad08aec9c81bd2508a6dc5f2a5eecd1f5c4f8451
Rick van bodegraven <rickvanbodegraven@gmail.com>
2012-05-29 15:36:32 +0200

    I be addin bro

--- java6eetut/src/converter/ejb/ConverterBean.java
+++ java6eetut/src/converter/ejb/ConverterBean.java
@@ -1,20 +1,26 @@
 package converter.ejb;
 
 import java.math.BigDecimal;
-import javax.ejb.*;
 
+
+/**
+ * This is the bean class for the ConverterBean enterprise bean.
+ * @author ian
+ */
 @Stateless
 public class ConverterBean {
-    private BigDecimal yenRate = new BigDecimal("83.0602");
-    private BigDecimal euroRate = new BigDecimal("0.0093016");
+    private BigDecimal euroRate = new BigDecimal("0.0078");
+    private BigDecimal yenRate = new BigDecimal("96.0650");
 
     public BigDecimal dollarToYen(BigDecimal dollars) {
         BigDecimal result = dollars.multiply(yenRate);
+
         return result.setScale(2, BigDecimal.ROUND_UP);
     }
 
     public BigDecimal yenToEuro(BigDecimal yen) {
         BigDecimal result = yen.multiply(euroRate);
+
         return result.setScale(2, BigDecimal.ROUND_UP);
     }
 }
